4dfb27c62bc929f4c51093cc13dff7024e557154
Craig Condon <craig@spiceapps.com>
2011-07-30 01:19:04 -0500

    simplified

---
+++ examples/interception/index.js
@@ -0,0 +1,32 @@
+var beanpole = require('../../lib/node').router();
+	
+function intercept(pull)
+{
+
+	//change the data
+	pull.data.sport = 'soccer';
+
+	pull.next();
+}
+
+function test(pull)
+{
+	pull.end(pull.data.sport)
+}
+
+function init()
+{
+	beanpole.pull('test', { sport: 'football' }, function(result)
+	{
+		console.log(result)
+	});
+}
+
+beanpole.on({
+	'push init': init,
+	'pull -intercept=sport test/interception': intercept,
+	'pull test': test
+})
+
+
+beanpole.push('init');
