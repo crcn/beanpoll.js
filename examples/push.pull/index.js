39ece4aa9e9e4ad8d7fe9bee314aea29e46a96a4
Craig Condon <craig@spiceapps.com>
2011-08-04 13:21:51 -0500

    testing push/pull

--- examples/push.pull/index.js
+++ examples/push.pull/index.js
@@ -7,15 +7,36 @@ beanpole.on({
 	{
 		setTimeout(function()
 		{
+			beanpole.push('name', function()
+			{
+				return "SLAYERS!";
+			});
+
 			beanpole.push('name', 'PUSH');
-			beanpole.push('name', 'PUSH');
-			beanpole.push('name', 'PUSH');
+			// beanpole.push('name', 'PUSH');
+			// beanpole.push('name', 'PUSH');
+			// beanpole.push('name', 'PUSH');
+			// beanpole.push('name', 'PUSH');
 		}, 100);
 
 		return 'PULL';
 	},
-	'push -pull name': function(name)
+	'push /*': function()
+	{
+		console.log("PUSH INITIALIZED!");
+		this.next();
+	},
+	'push through': function(data)
+	{
+		console.log("delay!")
+		setTimeout(function(self){ self.next(); }, 500, this)
+	},
+	'push -pull through -> name': function(name)
 	{
 		console.log(name)
-	}
+	},
+	/*'push name': function(name)
+	{
+		console.log(name +' PUSH')
+	}*/
 });
