fdafc24c2e7257097cc9b4ff9cf88c4f0a7dfc61
Craig Condon <craig@spiceapps.com>
2011-08-09 10:28:34 -0500

    testing root middleware

--- examples/or.hello/index.js
+++ examples/or.hello/index.js
@@ -15,11 +15,18 @@ beanpole.on({
 	'push -filter=GOLD blah': function()
 	{
 		console.log("GO")
+	},
+
+	'pull /*': function()
+	{
+		console.log("G")
 	}
 })
+
 
 
 beanpole.push('say/hello');
 beanpole.push('say/hello/world');
 beanpole.push('-filter=GOLD blah');
 beanpole.push('-filter=BOMB blah');
+beanpole.push('/');
