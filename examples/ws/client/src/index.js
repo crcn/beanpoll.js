1d644dda798de67e9ec3c4add7830c7241c7f3fd
Craig Condon <craig@spiceapps.com>
2011-08-11 16:57:47 -0500

    updated to better handle disconnects

--- examples/ws/client/src/index.js
+++ examples/ws/client/src/index.js
@@ -1,44 +1,68 @@
 var beanpole = require('../../../../lib/core').router();
 
+
+
+function pluginExample(router)
+{
+
+	var name = prompt("What's your name?", 'craig');
+	var time = Number(prompt("When do you want an alert? (in seconds)", 1));
+
+
+	function appendBody(message)
+	{
+		var div = document.createElement('div');
+		div.innerHTML = message;
+		document.body.appendChild(div);
+	}
+
+
+	router.on({
+
+		/**
+		 */
+
+		'pull -public some/random/callback': function(request)
+		{
+			appendBody(request.data.message);
+
+			this.from.push('notify/clients', request.data);
+
+			request.end();
+		},
+
+		/**
+		 */
+
+
+		'push send/message': function(data)
+		{
+
+			beanpole.push('call/later', { channel: 'some/random/callback', data: { _id: new Date().getTime(), message: data.message }, sendAt: new Date().getTime() + data.delay})
+		},
+
+		/**
+		 */
+
+		'push -public notify/clients': function(data)
+		{
+			appendBody('notified from another client: '+ data.message);
+		}
+	});
+
+
+	beanpole.on({
+		'push -public spice.io/ready': function()
+		{
+			beanpole.push('send/message', { message: "hello " + name +"!", name: name, delay: time * 1000});
+		}
+	});	
+}
+
+
 require('../../../../lib/core/beans/hook.core').plugin(beanpole);
 require('../../../../lib/web/beans/hook.socket.io').plugin(beanpole);
-
-beanpole.on({
-	 
-	/**
-	 */
-
-	'push -public spice.io/ready': function()
-	{ 
-		console.log('ready!');
-
-		console.log(beanpole.channels());
-
-		for(var i = 10; i--;)
-		beanpole.push('add/thyme', { channel: 'some/random/callback', data: { _id: new Date().getTime() + 2000, message: 'hello world!!!!!!' }})
-	},
-
-	/**
-	 */
-
-	'pull -public some/random/callback': function(request)
-	{
-		console.log("CALLBACK");
-
-		console.log(this.data);
-
-
-		request.end();
-	},
-
-	/** 
-	 */
-	 
-	'pull -public get/name': function()
-	{
-		return "CRAAAAAIIIGGGG!!";
-	}
-});
+pluginExample(beanpole);
 
 beanpole.push('ready','client');
 beanpole.push('init');   
